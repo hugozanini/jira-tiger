b1fb8169970928a08ef532e396d7fb1dd09b9454
--unset <hugozaninigomes@gmail.com>
2024-12-31 17:15:45 -0300

    Weekly analysis adjusted

--- src/agents/tools/weekly_analysis.py
+++ src/agents/tools/weekly_analysis.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel, Field, ConfigDict
 
 class JiraWeeklyDataAnalysisSchema(BaseModel):
     """Schema for JiraStorageTools inputs"""
-    action: str = Field(description="Action to perform. Possible actions: get_current_week, get_previous_week, list_available_weeks, get_data_for_month, read_weekly_data")
+    action: str = Field(description="Action to perform. Possible actions: get_weekly_report")
     current_week_path: str = Field(default=None, description="Path for the current week data")
     previous_week_path: str = Field(default=None, description="Path for the previous week data")
 
@@ -19,17 +19,36 @@ class JiraWeeklyDataAnalysis(BaseTool):
     args_schema: type[BaseModel] = JiraWeeklyDataAnalysisSchema
     df_current_week: pd.DataFrame = Field(default_factory=pd.DataFrame)
     df_previous_week: pd.DataFrame = Field(default_factory=pd.DataFrame)
+    current_week_path: str = "Current week path"
+    previous_week_path: str = "Previous week path"
 
-    def _run(self, current_week_path: str, previous_week_path: str) -> str:
+    def _run(self, action:str, current_week_path: str, previous_week_path: str) -> str:
+        """
+        Execute data analysis actions
+        Parameters:
+        action: str: The action to perform. Possible actions: "get_weekly_report"
+        current_week_path (str): Path for the current week data.
+        previous_week_path (str): Path for the previous week data.
+        """
+
+        actions = {
+            "get_weekly_report": self._generate_markdown
+        }
+
+        if action not in actions:
+            return f"Invalid action. Available actions: {list(actions.keys())}"
+
         self.current_week_path = current_week_path
         self.previous_week_path = previous_week_path
         self.df_current_week = None
         self.df_previous_week = None
 
-        report_path = self.generate_markdown_report()
+        return actions[action]()
 
-        return report_path
 
+    def _generate_markdown(self) -> str:
+        report_path, report = self.generate_markdown_report()
+        return report_path #report
 
     def add_last_update_columns(self, df):
         """Adds last_update_date and last_update columns based on the most recent comment"""
@@ -163,4 +182,4 @@ class JiraWeeklyDataAnalysis(BaseTool):
         with open(output_path, 'w') as f:
             f.write("\n".join(report))
 
-        return os.path.abspath(output_path)
+        return os.path.abspath(output_path), report
